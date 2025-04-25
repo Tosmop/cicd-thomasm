dd0099d2bbd09552d247a2976bf8cf6b3f7d91c7
38bobo56 <38bobo56@gmail.com>
2025-04-25 14:11:44 +0200

    feat: Added filters for product - Thomas M

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import redirect
 from django.views.generic import ListView
-
 from products.models import Product
 
 
@@ -10,12 +9,31 @@ def RedirectHomeView(request):
     '''
     return redirect('home')
 
+
 class HomeView(ListView):
     '''
     Renders home page with all the products
     '''
     template_name = 'home.html'
     model = Product
+    context_object_name = 'object_list'
 
-    def get(self, request):
-        return super().get(request)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+        sort_order = self.request.GET.get('sort')
+
+        # Filtrage par prix
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+
+        # Tri par ordre croissant ou décroissant
+        if sort_order == 'asc':
+            queryset = queryset.order_by('price')
+        elif sort_order == 'desc':
+            queryset = queryset.order_by('-price')
+
+        return queryset
